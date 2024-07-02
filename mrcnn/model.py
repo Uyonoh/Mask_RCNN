dd9b6ae9f6a65690ac97fab93b0f2b54213a2995
Uyonoh <turakiuyonoh@gmail.com>
2024-07-02 17:26:53 +0100

    correct hdf5 reference

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2158,9 +2158,9 @@ class MaskRCNN(object):
             layers = filter(lambda l: l.name not in exclude, layers)
 
         if by_name:
-            saving.load_weights_from_hdf5_group_by_name(f, layers)
+            hdf5_format.load_weights_from_hdf5_group_by_name(f, layers)
         else:
-            saving.load_weights_from_hdf5_group(f, layers)
+            hdf5_format.load_weights_from_hdf5_group(f, layers)
         if hasattr(f, 'close'):
             f.close()
 
